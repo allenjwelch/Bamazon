f34705b4a6107542f89001a167344f39fc8094e9
Allen Welch <allenjwelch@gmail.com>
2018-05-03 18:47:10 -0400

    completed manager.js -- issue with Add Inventory feature

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql'); 
 const menu = require('inquirer'); 
 const addIn = require('inquirer'); 
+const addProd = require('inquirer'); 
 const figlet = require('figlet'); 
+let newQuant = ''; 
+let id = ''; 
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -21,9 +24,9 @@ const pool = mysql.createPool({
 
 
 bamazonFig(); 
-setTimeout(menu, 1000); 
+setTimeout(menuQs, 1000); 
 
-function menu() {
+function menuQs() {
   menu.prompt([
     {
       type: 'list', 
@@ -94,38 +97,61 @@ function addInventory() {
       if (err) throw err; 
       connection.query(`SELECT * FROM products WHERE item_id = ${addItem.itemId}`, function(err,res) {
         if (err) throw err; 
-        let newQuant = addItem.itemAdd + res[0].stock_quantity; 
-
-        
-        console.log('=================================================\n');
+        newQuant = parseFloat(addItem.itemAdd) + res[0].stock_quantity;  
+        id = addItem.itemId; 
+        console.log(`Item: ${res[0].product_name}\nCurrent Quantitiy: ${res[0].stock_quantity}\nUpdated Quantity:${newQuant}`); 
       }); //connection.query
-      connection.end();  
+      connection.release();  
+      setTimeout(updateInvent,500); 
     }); // pool.getConnection
+  }); 
 
-
-
-    ///////////////////////////////////
-    // connection.query(`SELECT * FROM products`, function(err,res) {
-    //   if (err) throw err; 
-    //   console.log('\n=================================================');
-    //   res.forEach(item => {
-    //     console.log(`Id: ${item.item_id} || Item: ${item.product_name} || Price: $${item.price} || Quantity: ${item.stock_quantity}`); 
-    //   }); // res.forEach
-    //   console.log('=================================================\n');
-    // }); //connection.query
-    // connection.end();  
-    ///////////////////////////////////
-
-
-
-  })
-
-  
 }; //addInventory
+
+function updateInvent() {
+  connection.query(`UPDATE products SET stock_quantity = ${newQuant} WHERE item_id = ${id}`, function(err,res) {
+    if (err) throw err; 
+    console.log(`Item updated!`);
+  }); //connection.query
+  connection.end(); 
+
+  // setTimeout(endAll,500); 
+}
 
 function addProduct() {
   // If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+  addProd.prompt([
+    {
+      type: 'input', 
+      message: 'Enter the a name of the new item:', 
+      name: 'itemName'
+    },{
+      type: 'list', 
+      message: 'What department is this item with?:', 
+      choices: ['Outdoors', 'Household', 'Electronics', 'Clothing'],
+      name: 'itemDep'
+    },{
+      type: 'input', 
+      message: 'What is the price of your item?', 
+      name: 'itemPrice'
+    },{
+      type: 'input', 
+      message: 'How many items are being stocked?', 
+      name: 'itemStock'
+    },
+  ]).then(product => {
+    let price = parseFloat(product.itemPrice); 
+    let stock = parseFloat(product.itemStock);
+    connection.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('${product.itemName}', '${product.itemDep}', ${price}, ${stock})`, function(err,res) {
+      if (err) throw err; 
+      console.log(`Item Added!`);
+    }); //connection.query
+    connection.end(); 
+  }); 
+}; 
 
+function endAll() {
+  connection.end(); 
 }
 
 function bamazonFig() {
