76aba2ce41e947459e21cdbdfbf678545b6e5551
Allen Welch <allenjwelch@gmail.com>
2018-05-02 20:04:43 -0400

    updated manager functions - viewAll and viewLow

--- README.md
+++ README.md
@@ -11,3 +11,12 @@
 - Customer enters item id and desired quantity
   _*If quantity desired exceeds what is in stock, customer will be asked to select again_
 - Once customer purchase is complete, the Bamazon DataBase is updated and purchase information is displayed
+
+
+![ManagerView](managerDisplay.gif)
+
+NEEDED: 
+ - Add an 'Exit' menu item to initial prompt. 
+ - Have all menu functions return back to initial prompt.
+
+ -- May have to use just connection instead of pool -- 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,20 +1,38 @@
 const mysql = require('mysql'); 
-const initial = require('inquirer'); 
+const menu = require('inquirer'); 
+const addIn = require('inquirer'); 
+const figlet = require('figlet'); 
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306, 
+  user: 'root', 
+  password: 'root', 
+  database: 'bamazon_db'
+}); 
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306, 
+  user: 'root', 
+  password: 'root', 
+  database: 'bamazon_db'
+});
 
 
-bamzonFig(); 
-setTimeout(initialize, 1000); 
+bamazonFig(); 
+setTimeout(menu, 1000); 
 
-function initialize() {
-  initial.prompt([
+function menu() {
+  menu.prompt([
     {
       type: 'list', 
       message: 'Choose from the options below:', 
       choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
       name: 'menuItem'
     }
-  ]).then(initialInput => {
-    switch (initialInput.menuItem) {
+  ]).then(menuInput => {
+    switch (menuInput.menuItem) {
       case 'View Products for Sale':
         viewAllProducts(); 
         break; 
@@ -31,26 +49,83 @@ function initialize() {
         console.log('How did you even select that?')
     }; // switch 
   }); //initial.prompt
-}; //initialize
+}; //menu
 
 function viewAllProducts() {
   // If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
-
-}
+  connection.query(`SELECT * FROM products`, function(err,res) {
+    if (err) throw err; 
+    console.log('\n=================================================');
+    res.forEach(item => {
+      console.log(`Id: ${item.item_id} || Item: ${item.product_name} || Price: $${item.price} || Quantity: ${item.stock_quantity}`); 
+    }); // res.forEach
+    console.log('=================================================\n');
+  }); //connection.query
+  connection.end();  
+}; // viewAllProducts
 
 function viewLowInventory() {
   // If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
-
-}
+  connection.query(`SELECT * FROM products WHERE stock_quantity < 5`, function(err,res) {
+    if (err) throw err; 
+    console.log('\n=================================================');
+    res.forEach(item => {
+      console.log(`Id: ${item.item_id} || Item: ${item.product_name} || Price: $${item.price} || Quantity: ${item.stock_quantity}`); 
+    }); // res.forEach
+    console.log('=================================================\n');
+  }); //connection.query
+  connection.end();  
+}; // viewLowInventory
 
 function addInventory() {
   // If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+  addIn.prompt([
+    {
+      type: 'input', 
+      message: 'Enter the Id# of the Item:', 
+      name: 'itemId'
+    }, {
+      type: 'input', 
+      message: 'How many units of this item would you like to add?', 
+      name: 'itemAdd'
+    }
+  ]).then(addItem => {
+    pool.getConnection(function(err, connection) {
+      if (err) throw err; 
+      connection.query(`SELECT * FROM products WHERE item_id = ${addItem.itemId}`, function(err,res) {
+        if (err) throw err; 
+        let newQuant = addItem.itemAdd + res[0].stock_quantity; 
 
-}
+        
+        console.log('=================================================\n');
+      }); //connection.query
+      connection.end();  
+    }); // pool.getConnection
+
+
+
+    ///////////////////////////////////
+    // connection.query(`SELECT * FROM products`, function(err,res) {
+    //   if (err) throw err; 
+    //   console.log('\n=================================================');
+    //   res.forEach(item => {
+    //     console.log(`Id: ${item.item_id} || Item: ${item.product_name} || Price: $${item.price} || Quantity: ${item.stock_quantity}`); 
+    //   }); // res.forEach
+    //   console.log('=================================================\n');
+    // }); //connection.query
+    // connection.end();  
+    ///////////////////////////////////
+
+
+
+  })
+
+  
+}; //addInventory
 
 function addProduct() {
   // If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
-  
+
 }
 
 function bamazonFig() {
